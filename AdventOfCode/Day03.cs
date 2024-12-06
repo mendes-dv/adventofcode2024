c7af03c6699ad26672800a3f19c9b520521cafc2
mendes <goncalo.mendes.dev@gmail.com>
2024-12-06 07:55:30 +0000

    Add input file 03.txt for AdventOfCode
    This commit adds a new input file, 03.txt, to the Inputs directory for the AdventOfCode project. The contents of the file seem formatted for a specific challenge or puzzle and may include various operations or commands. This file will be used as part of the code execution and problem-solving process.

---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode;
+
+public class Day03 : BaseDay
+{
+    private readonly string _input;
+
+    public Day03()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {Solver01()}, part 1");
+
+    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {Solver02()}, part 2");
+
+    private long Solver01()
+    {
+        string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+        MatchCollection matches = Regex.Matches(_input, pattern);
+
+        var total = 0;
+        foreach (Match match in matches)
+        {
+            string[] split = match.Value.Split(new char[] { '(', ',', ')' });
+            total += int.Parse(split[1]) * int.Parse(split[2]);
+        }
+
+        return total;
+    }
+
+    private long Solver02()
+    {
+        string pattern = @"mul\(\d{1,3},\d{1,3}\)|don't\(\)|do\(\)";
+        Regex regex = new Regex(pattern);
+
+        MatchCollection matches = regex.Matches(_input);
+
+        var output = 0;
+        var startStop = true;
+        foreach (Match match in matches) {
+            if (match.Value.Contains("don't()") || match.Value.Contains("do()")){
+                startStop = !match.Value.Contains("don't()");
+                continue;
+            }
+            if(!startStop){
+                continue;
+            }
+            string[] split = match.Value.Split(new char[] { '(', ',', ')' });
+            output += int.Parse(split[1]) * int.Parse(split[2]);
+        }
+        
+        return output;
+    }
+}
